037ca58de23a3f9e94a5272be6e65cea4815d50b
Shuaib <shuaibuddowla.personal@gmail.com>
2026-07-01 21:22:29 +0600

    chore: bump version to 1.1.5 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.shuaib.classmate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.4"
+        versionCode = 6
+        versionName = "1.1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "GITHUB_LIBRARY_TOKEN", "\"${getLocalProperty("GITHUB_LIBRARY_TOKEN")}\"")
